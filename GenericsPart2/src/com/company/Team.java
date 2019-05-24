0a5b37b2c366f11e320ca76ffbaaae6edbc14290
Junpengalaitp <hejunpeng2012@hotmail.com>
2019-05-24 15:01:48 +0800

    packages 1

---
+++ GenericsPart2/src/com/company/Team.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public class Team {
+    private String name;
+}
---
+++ Packages/src/app/App.java
@@ -0,0 +1,10 @@
+package app;
+
+import javax.xml.soap.Node;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Node node = null;
+    }
+}
