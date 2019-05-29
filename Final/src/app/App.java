c1d506764cb40fd44334a25363d25fa841ee10f9
Junpengalaitp <hejunpeng2012@hotmail.com>
2019-05-29 15:43:14 +0800

    Final Part 2

--- Final/src/app/App.java
+++ Final/src/app/App.java
@@ -2,26 +2,29 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        SomeClass one = new SomeClass("one");
-        SomeClass two = new SomeClass("two");
-        SomeClass three = new SomeClass("three");
+        // SomeClass one = new SomeClass("one");
+        // SomeClass two = new SomeClass("two");
+        // SomeClass three = new SomeClass("three");
 
-        System.out.println(one.getInstanceNumber());
-        System.out.println(two.getInstanceNumber());
-        System.out.println(three.getInstanceNumber());
+        // System.out.println(one.getInstanceNumber());
+        // System.out.println(two.getInstanceNumber());
+        // System.out.println(three.getInstanceNumber());
 
-        // one.instanceNumber = 4;
-        System.out.println(Math.PI);
-        // Math m = new Math();
+        // // one.instanceNumber = 4;
+        // System.out.println(Math.PI);
+        // // Math m = new Math();
 
-        int pw = 674312;
-        Password password = new Password(pw);
-        password.storePassword();
+        // int pw = 674312;
+        // Password password = new ExtendedPassword(pw);
+        // password.storePassword();
 
-        password.letMeIn(1);
-        password.letMeIn(523266);
-        password.letMeIn(9733);
-        password.letMeIn(674312);
-
+        // password.letMeIn(1);
+        // password.letMeIn(523266);
+        // password.letMeIn(9733);
+        // password.letMeIn(674312);
+        System.out.println("Main method called");
+        SIBTest test = new SIBTest();
+        test.someMethod();
+        System.out.println("Owner is " + SIBTest.owner);
     }
 }
